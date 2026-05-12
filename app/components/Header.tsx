7ba4197269661b5056df969430127a1aa114fb80
Kasun Lakmal <kasunlakmal20487ks1@gmail.com>
2026-05-12 17:07:05 +0530

    build-fix: use auth logic instead of signed-in tags

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -24,7 +24,7 @@ export default async function Header() {
           </Link>
           
           {userId ? (
-            <UserButton afterSignOutUrl="/" />
+            <UserButton />
           ) : (
             <SignInButton mode="modal">
               <button className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary">
